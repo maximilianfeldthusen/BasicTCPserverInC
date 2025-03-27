0ec13a773668b26cdf817a916f1eb84ac7cfedaa
Maximilian Feldthusen <96908021+maximilianfeldthusen@users.noreply.github.com>
2025-03-27 04:55:52 +0100

    Update tcpserver.c

--- tcpserver.c
+++ tcpserver.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,6 +9,7 @@
 
 #define PORT 8080
 #define BUFFER_SIZE 1024
+#define BACKLOG 3  // Maximum number of pending connections
 
 int main() {
     int server_fd, new_socket;
@@ -35,31 +37,48 @@ int main() {
     }
 
     // Listening for incoming connections
-    if (listen(server_fd, 3) < 0) {
+    if (listen(server_fd, BACKLOG) < 0) {
         perror("Listen failed");
         close(server_fd);
         exit(EXIT_FAILURE);
     }
 
-    printf("Server is listening on port %d\n", PORT);
+    printf("Server is listening on port %d
+", PORT);
 
-    // Accepting a connection
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen)) < 0) {
-        perror("Accept failed");
-        close(server_fd);
-        exit(EXIT_FAILURE);
+    while (1) {
+        // Accepting a connection
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen)) < 0) {
+            perror("Accept failed");
+            continue; // Continue to accept other connections
+        }
+
+        // Reading data from the client
+        ssize_t bytes_read = recv(new_socket, buffer, BUFFER_SIZE - 1, 0);
+        if (bytes_read < 0) {
+            perror("Read failed");
+            close(new_socket);
+            continue; // Continue to accept other connections
+        }
+        buffer[bytes_read] = '\0'; // Null terminate the string
+        printf("Message from client: %s
+", buffer);
+
+        // Sending response to the client
+        ssize_t bytes_sent = send(new_socket, response, strlen(response), 0);
+        if (bytes_sent < 0) {
+            perror("Send failed");
+        } else {
+            printf("Response sent to client
+");
+        }
+
+        // Closing the socket
+        close(new_socket);
     }
 
-    // Reading data from the client
-    read(new_socket, buffer, BUFFER_SIZE);
-    printf("Message from client: %s\n", buffer);
-
-    // Sending response to the client
-    send(new_socket, response, strlen(response), 0);
-    printf("Response sent to client\n");
-
-    // Closing the socket
-    close(new_socket);
+    // Closing the server socket (unreachable in current loop)
     close(server_fd);
     return 0;
 }
+
